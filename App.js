efe4a7eab37ec078717082b206e95ec1f714bb55
Yan Carlos <yancarlos55@yahoo.com.br>
2022-03-29 10:55:24 -0300

    Continuação da Aula

--- App.js
+++ App.js
@@ -1,37 +1,53 @@
-
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import Botao from "./src/components/botao";
 import Cabecalho from "./src/components/cabecalho";
-
+import Jogos from "./src/components/Jogos";
+import dados from "./dados";
 
 export default function App() {
   return (
-    <View style={{backgroundColor:'#D3D3D3', flex:1}}>
+    <View style={{ backgroundColor: "#D3D3D3", flex: 1 }}>
       <Cabecalho></Cabecalho>
-      <Botao 
+      <Botao
         Logo="logo-android"
         texto="APLICATIVOS"
         cor="#9FE2BF"
-        Logo2="game-controller" 
-        texto2="JOGOS" 
+        Logo2="game-controller"
+        texto2="JOGOS"
         cor2="#820066"
       />
-      <Botao 
+      <Botao
         Logo="film"
         texto="FILMES"
         cor="#FF7F50"
-        Logo2="musical-notes" 
-        texto2="MUSICAS" 
+        Logo2="musical-notes"
+        texto2="MUSICAS"
         cor2="#6495ED"
       />
-      <Botao 
+      <Botao
         Logo="book"
         texto="LIVROS"
         cor="#CCCCFF"
-        Logo2="journal" 
-        texto2="NOTICIAS" 
+        Logo2="journal"
+        texto2="NOTICIAS"
         cor2="#FFBF00"
       />
-     </View>
+
+      <FlatList 
+        horizontal={true} 
+        data={dados} 
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+
+          <Jogos
+            titulo={item.nome}
+            valor={item.valor}
+            imagem={item.imagem}
+          />
+        )}
+        />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({});
---
+++ dados/index.js
@@ -0,0 +1,36 @@
+import Jogos from "../src/components/Jogos";
+
+const JOGOS = [
+    {
+        id: "01",
+        nome: "Subway Surfers",
+        valor: 13.32,
+        imagem: "subway.jpg",
+    },
+    {
+        id: "02",
+        nome: "Yugioh Master Duel",
+        valor: 0,
+        imagem: "yugi.jpg",
+    },
+    {
+        id: "03",
+        nome: "Pokemon Go",
+        valor: 0,
+        imagem: "pokemon.jpg",
+    },
+    {
+        id: "04",
+        nome: "Hora de Aventura",
+        valor: 10,
+        imagem: "aventura.jpg",
+    },
+    {
+        id: "05",
+        nome: "Dragon Ball Fight",
+        valor: 18,
+        imagem: "dbz.jpg",
+    },
+]
+
+export default JOGOS;
